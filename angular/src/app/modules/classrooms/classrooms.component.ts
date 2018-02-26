95562d054fdfa8705db9581d39eea23d0f86f78b
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-26 15:50:50 -0500

    Fix collapsing wrong card

--- angular/src/app/modules/classrooms/classrooms.component.ts
+++ angular/src/app/modules/classrooms/classrooms.component.ts
@@ -17,8 +17,8 @@ export class ClassroomsComponent implements OnInit {
   constructor() {
     // create mock data
     let class0 = new Classroom(0, "Class-0", "OPEN", 0, 30);
-    let class1 = new Classroom(0, "Class-1", "UNAVAILABLE", 12, 30);
-    let class2 = new Classroom(0, "Class-2", "RESERVED", 10, 20);
+    let class1 = new Classroom(1, "Class-1", "UNAVAILABLE", 12, 30);
+    let class2 = new Classroom(2, "Class-2", "RESERVED", 10, 20);
 
     this.classroomList = [class0, class1, class2];
   }
