5d2b136f4df87d23db06bbfd72ae38c478f06bd4
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-26 14:49:29 -0500

    Added Material Icons

--- angular/src/app/modules/classrooms/classrooms.component.ts
+++ angular/src/app/modules/classrooms/classrooms.component.ts
@@ -14,9 +14,9 @@ export class ClassroomsComponent implements OnInit {
 
   constructor() {
     // create mock data
-    let class0 = new Classroom(0, "Class-0", "OPENED", 0, 30);
-    let class1 = new Classroom(0, "Class-1", "RESERVED", 12, 30);
-    let class2 = new Classroom(0, "Class-2", "OPENED", 0, 20);
+    let class0 = new Classroom(0, "Class-0", "OPEN", 0, 30);
+    let class1 = new Classroom(0, "Class-1", "UNAVAILABLE", 12, 30);
+    let class2 = new Classroom(0, "Class-2", "RESERVED", 10, 20);
 
     this.classroomList = [class0, class1, class2];
   }
