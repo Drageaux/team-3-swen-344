eeae5fe1635b10a1f8cb150b2a0dc9ba3c77dd4d
David Thong Nguyen <thnguyen2808@gmail.com>
2018-03-08 17:46:09 -0500

    External TweetModule rendering well

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {HttpModule} from '@angular/http';
-import { Router } from '@angular/router';
 import { appRoutingProviders, routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './not-found.component';
 
 import { SampleModule } from './modules/sample/sample.module';
-import { ClassroomsModule } from './modules/classrooms/classrooms.module';
 import { HomeModule } from './modules/home/home.module';
-
+import { ClassroomsModule } from './modules/classrooms/classrooms.module';
 import { DevicesModule } from './modules/devices/devices.module';
-import { DevicesService } from './modules/devices/devices.service';
 
 
 @NgModule({
@@ -26,7 +22,6 @@ import { DevicesService } from './modules/devices/devices.service';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    HttpModule,
     routing,
     SampleModule,
     HomeModule,
--- angular/src/app/modules/home/home.module.ts
+++ angular/src/app/modules/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { TweetModule } from 'ngx-tweet/lib/ngx-tweet.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TweetModule
   ],
   declarations: [HomeComponent]
 })
