1df3f26c8c36bcbf642b9f37a4ce568b7238d1ac
David Thong Nguyen <thnguyen2808@gmail.com>
2018-03-08 18:17:21 -0500

    Tests work; classroomsController works alot like devicesController now

--- unit_tests/classroomsUnitTests.js
+++ unit_tests/classroomsUnitTests.js
@@ -1,41 +1,176 @@
 var request = require('supertest');
 var should = require('should');
-describe('Testing GET classroom API', function () {
+
+describe('Testing GET classrooms API', function () {
     var server;
     before(function () {
         server = require('../app');
     });
-    //INSERT IN UNIT TESTS
+    it('responds to get /api/classrooms/', function testSlash(done) {
+        request(server)
+            .get('/api/classrooms/')
+            .expect(200, done);
+    });
+    it('responds to GET with id /api/classrooms/1', function testSlash(done) {
+        request(server)
+            .get('/api/classrooms/1')
+            .expect(200, done);
+    });
+    it('Sends back 500 with improper id /api/classrooms/-1', function testSlash(done) {
+        request(server)
+            .get('/api/classrooms/-1')
+            .expect(500, done);
+    });
+    it('Sends back 500 with non-numeric id /api/classrooms/abcd', function testSlash(done) {
+        request(server)
+            .get('/api/classrooms/abcd')
+            .expect(500, done);
+    });
+    it('Sends back 500 with no messages found /api/classrooms/100', function testSlash(done) {
+        request(server)
+            .get('/api/classrooms/100')
+            .expect(500, done);
+    });
+    after(function () {
+        server.close();
+    });
     after(function () {
         server.close();
     });
 });
-describe('Testing POST classroom API', function () {
+describe('Testing POST classrooms API', function () {
     var server;
     before(function () {
         server = require('../app');
     });
-    //INSERT IN UNIT TESTS
+    it('responds to POST /api/classrooms/', function testSlash(done) {
+        request(server)
+            .post('/api/classrooms/')
+            .send({ "name": "Foo" })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                if (err) return done(err);
+                res.body.should.be.instanceOf(Object);
+                done();
+            })
+    });
+    it('responds to POST /api/classrooms/ with missing field', function testSlash(done) {
+        request(server)
+            .post('/api/classrooms/')
+            .send({})
+            .expect(500)
+            .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            })
+    });
     after(function () {
         server.close();
     });
 });
-describe('Testing PUT classroom API', function () {
+describe('Testing PUT classrooms API', function () {
     var server;
     before(function () {
         server = require('../app');
     });
-    //INSERT UNIT TESTS IN HERE
+    it('responds to PUT /api/classrooms/', function testSlash(done) {
+        request(server)
+            .put('/api/classrooms/')
+            .send({ "id": 1, "name": "Foo" })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                if (err) return done(err);
+                res.body[1].should.have.property('name').which.equals("Foo");
+                done();
+            })
+    });
+    it('responds to PUT /api/classrooms/ with missing field', function testSlash(done) {
+        request(server)
+            .put('/api/classrooms/')
+            .send({ "id": 1 })
+            .expect(500)
+            .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responds to PUT /api/classrooms/ with non-integer id', function testSlash(done) {
+        request(server)
+            .put('/api/classrooms/')
+            .send({ "id": "abc", "name": "Foo" })
+            .expect(500)
+            .end(function (err, res) {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responds to PUT /api/classrooms/ with negative id', function testSlash(done) {
+        request(server)
+            .put('/api/classrooms/')
+            .send({ "id": -1, "name": "Foo" })
+            .expect(500)
+            .end(function (err, res) {
+                done();
+            })
+    });
+    it('responds to PUT /api/classrooms/ with classroom that does not exist', function testSlash(done) {
+        request(server)
+            .put('/api/classrooms/')
+            .send({ "id": 100, "name": "Foo" })
+            .expect(500)
+            .end(function (err, res) {
+                done();
+            })
+    });
     after(function () {
         server.close();
     });
 });
-describe('Testing DELETE classroom API', function () {
+describe('Testing DELETE classrooms API', function () {
     var server;
     before(function () {
         server = require('../app');
     });
-    //INSERT UNIT TESTS IN HERE
+    it('responds to DELETE /api/classrooms/', function testSlash(done) {
+        request(server)
+            .delete('/api/classrooms/0')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body[0].should.have.property('id').and.is.equal(1);
+                done();
+            })
+    });
+    it('responds to DELETE /api/classrooms/ with negative id', function testSlash(done) {
+        request(server)
+            .delete('/api/classrooms/-1')
+            .expect(500)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            })
+    });
+    it('responds to DELETE /api/classrooms/ with non-integer id', function testSlash(done) {
+        request(server)
+            .delete('/api/classrooms/abcd')
+            .expect(500)
+            .end(function(err, res) {
+                if (err) done(err);
+                done();
+            })
+    });
+    it('responds to DELETE /api/classrooms/ with classroom that does not exist', function testSlash(done) {
+        request(server)
+            .delete('/api/classrooms/100')
+            .expect(500)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            })
+    });
     after(function () {
         server.close();
     });
