05c17ddf1905109b2784ade4fbb4da4fecb09a58
David Thong Nguyen <thnguyen2808@gmail.com>
2018-03-07 23:48:27 -0500

    Prevent checkbox event from "bubbling" up to the entire row (since they're overlapping)

--- angular/src/app/modules/messaging/messaging.component.ts
+++ angular/src/app/modules/messaging/messaging.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
   selector: 'app-messaging',
   templateUrl: './messaging.component.html',
@@ -9,9 +11,21 @@ export class MessagingComponent implements OnInit {
 
   private messageOpen: boolean = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    // prevent checkbox event from "bubbling" up to the entire row (since they're overlapping)
+    $('#inbox input[type=checkbox]').click(function (event) {
+      console.log('test')
+      if (event.stopPropagation) {
+        // standard
+        event.stopPropagation();
+      } else {
+        // IE6-8
+        event.cancelBubble = true;
+      }
+    });
   }
 
 }
