c129b4537cb51c73332fdd7d6ae29463be069143
David Thong Nguyen <thnguyen2808@gmail.com>
2018-04-08 17:29:02 -0400

    Added missing provider declaration

--- angular/src/app/modules/rentals/rentals.module.ts
+++ angular/src/app/modules/rentals/rentals.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RentalsComponent } from './rentals.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RentalsComponent} from './rentals.component';
+import {RentalsService} from "./rentals.service";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [RentalsComponent]
+  declarations: [RentalsComponent],
+  providers: [RentalsService]
 })
-export class RentalsModule { }
+export class RentalsModule {
+}
