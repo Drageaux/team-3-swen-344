ec1e3ad5e3066a62d3e812a209d6f72d189608c5
David Thong Nguyen <thnguyen2808@gmail.com>
2018-04-08 16:34:31 -0400

    Changed DeviceName column properties

--- models/deviceName.js
+++ models/deviceName.js
@@ -1,12 +1,17 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var DeviceName = sequelize.define('DeviceName', {
         // id: DataTypes.UUID,
         name: DataTypes.STRING
     });
 
-    DeviceName.associate = function(models) {
-        models.DeviceName.belongsTo(models.Device);
+    DeviceName.associate = function (models) {
+        models.DeviceName.belongsTo(models.Device, {
+            onDelete: "CASCADE",
+            foreignKey: {
+                allowNull: false
+            }
+        });
     };
 
     return DeviceName;
