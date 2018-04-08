2035fabb4b524c8715b9d9980433ece764c7dd4f
JetMas <jethromasangya@gmail.com>
2018-04-07 22:42:37 -0400

    Created helper function for accessing rentas

---
+++ controllers/rentalsController.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var rentalsController = express.Router();
+
+let data = {
+  users: [
+    {id:0, username:"abc1234"},
+    {id:1, username:"dpo5678"},
+    {id:2, username:"zxc0987"}
+  ]
+  devices: [
+    {id: 0, name: "Microscope", rentStatus: true},
+    {id: 1, name: "Laptop", rentStatus: true},
+    {id: 2, name: "Tape Measure", rentStatus: false}
+  ]
+  rentals: [
+    {id:0, deviceId:0,renterId:0,returnCondition:"good",comment:"none",rentDate:"2018-4-7T09:08:00",dueDate:"2018-4-14T09:00:00",returnDate:"2018-4-9T10:00:00"},
+    {id:1, deviceId:1,renterId:1,returnCondition:"brocken",comment:"heavily damaged",rentDate:"2018-4-7T09:08:00",dueDate:"2018-4-14T09:00:00",returnDate:"2018-4-9T10:00:00"}
+  ]
+};
+
+let currID = data.rentals.length;
+
+function findRentalByID(id) {
+  for (var i = 0; i < data.rentals.length; i++) {
+    if (data.rentals[i].id == id) {
+      return data.rentals[i];
+    }
+  }
+  return null;
+}
+
+function createNewRentals(deviceId, renterId, rentDate, dueDate) {
+  newRental = {
+    id:currID,
+    deviceId:deviceId,
+    renterId:renterId,
+    returnCondition:null,
+    comment:null,
+    rentDate:rentDate,
+    dueDate:dueDate,
+    returnDate:null
+  }
+  currID++;
+  data.renatals.push(newRental);
+  return newRental;
+}
+
+function returnRental(id, condition, comment, returnDate){
+  for (var i = 0; i < data.rentals.length; i++) {
+    if (data.rentals[i].id == id) {
+      var rental = data.rentals[i];
+      rental.condition = condition;
+      rental.comment = comment;
+      rental.returnDate = returnDate;
+      return rental;
+    }
+  }
+}
+
+//returns all reservations
+rentalsController.get('/', function (req, res) {
+    res.json(data.rental);
+});
+
+//Returns the reservation with of the requested id
+rentalsController.get('/:id', function (req, res) {
+    let rental = findRentalByID(req.params.id);
+    if (rental) {
+        res.json(rental);
+    }
+    else {
+        res.status(500).send("Cannot find rental.");
+    }
+});
+
+//create a new reservation
+rentalsController.post('/', function (req, res) {
+    if(req.body){
+        res.json(createNewRentals(req.body.classroomId, req.body.startDate, req.body.endDate, req.body.reservedBy, req.body.eventName));
+    }
+    else {
+        res.status(500).send("Bad Request");
+    }
+});
+
+//Cancels a reservation
+reservationsController.delete('/:id', function(req, res){
+    let reservation = findReservationByID(req.params.id);
+    if(reservation){
+        cancelReservation(req.params.id);
+        res.json(data.reservations);
+    }
+    else {
+        res.status(500).send("Bad Request");
+    }
+});
+
+module.exports = rentalsController;
