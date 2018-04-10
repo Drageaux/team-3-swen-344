ae11d7afb07000c401ed33bc0faa39678c9af146
David Thong Nguyen <thnguyen2808@gmail.com>
2018-04-10 15:52:25 -0400

    Updated User model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define('User', {
         authId: DataTypes.STRING,
+        role: DataTypes.ENUM('admin', 'instructor', 'proxy', 'student'),
+        name: DataTypes.STRING,
         email: DataTypes.STRING
     }, {});
     User.associate = function (models) {
